29348a1fc9a352e28806e63f070630878a0a299b
Chamali Vishmani <chamali106astro@gmail.com>
2025-01-02 15:45:20 +0530

    disconnect event listener

--- src/main/java/com/chamalidev/chatappwebsockets/chat/ChatMessage.java
+++ src/main/java/com/chamalidev/chatappwebsockets/chat/ChatMessage.java
@@ -1,14 +1,12 @@
 package com.chamalidev.chatappwebsockets.chat;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ChatMessage {
     private String content;
     private String sender;
--- src/main/java/com/chamalidev/chatappwebsockets/config/WebSocketEventListener.java
+++ src/main/java/com/chamalidev/chatappwebsockets/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.chamalidev.chatappwebsockets.config;
 
+import com.chamalidev.chatappwebsockets.chat.ChatMessage;
+import com.chamalidev.chatappwebsockets.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,9 +14,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @EventListener
     public void handleWebSockerDisconnectListener(SessionDisconnectEvent event){
-        //TODO : implement
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username!=null){
+            log.info("User disconnected : {}",username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            simpMessageSendingOperations.convertAndSend("/topic/public",chatMessage);
+        }
     }
 }
