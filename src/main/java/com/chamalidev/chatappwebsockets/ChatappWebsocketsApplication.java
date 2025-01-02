15b024c02be4a1f2e03d04002a70e3472decdba3
Chamali Vishmani <chamali106astro@gmail.com>
2025-01-02 13:02:29 +0530

    web socket config

---
+++ src/main/java/com/chamalidev/chatappwebsockets/ChatappWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.chamalidev.chatappwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappWebsocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatappWebsocketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chamalidev/chatappwebsockets/ChatappWebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamalidev.chatappwebsockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappWebsocketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
